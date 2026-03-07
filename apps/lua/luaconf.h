902f09ca9415de16a24987c0f1176a959f6dd6b6
MD. SABBIR HOSHEN HOWLADER <sabbirb228@gmail.com>
2026-03-07 23:25:24 +0600

    fix some issue that prosible !

--- apps/lua/luaconf.h
+++ apps/lua/luaconf.h
@@ -638,7 +638,7 @@
 
 #if !defined(LUA_USE_C89) && defined(__STDC_VERSION__) && \
     __STDC_VERSION__ >= 199901L
-#include <stdint.h>
+// #include <stdint.h> we cant use that becouse we dont have lib
 #if defined(INTPTR_MAX)  /* even in C99 this type is optional */
 #undef LUA_KCONTEXT
 #define LUA_KCONTEXT	intptr_t
