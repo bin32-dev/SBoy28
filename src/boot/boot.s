cb90526947e5d5a56e4565c837b91985903edd18
bin32-dev <sabbirb2528@gmail.com>
2026-03-11 13:46:03 -0400

    Integrate and harden EFI ISO build workflow

--- src/boot/boot.s
+++ src/boot/boot.s
@@ -1,6 +1,16 @@
 # -----------------------------
 # Kernel Loader (Flat Binary)
 # -----------------------------
+.set MULTIBOOT_MAGIC,  0x1BADB002
+.set MULTIBOOT_FLAGS,  0x00000003
+.set MULTIBOOT_CHECKSUM, -(MULTIBOOT_MAGIC + MULTIBOOT_FLAGS)
+
+.section .multiboot
+.align 4
+    .long MULTIBOOT_MAGIC
+    .long MULTIBOOT_FLAGS
+    .long MULTIBOOT_CHECKSUM
+
 .section .text
 .global _start
 
