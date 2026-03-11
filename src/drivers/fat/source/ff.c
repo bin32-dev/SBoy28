ff438e020093488b462dc42e598982f6ce94e748
bin32-dev <sabbirb2528@gmail.com>
2026-03-11 08:37:13 -0400

    Fix FatFs build without hosted string headers

--- src/drivers/fat/source/ff.c
+++ src/drivers/fat/source/ff.c
@@ -19,7 +19,7 @@
 /----------------------------------------------------------------------------*/
 
 
-#include <string.h>
+#include "common/utils.h"
 #include "ff.h"			/* Basic definitions and declarations of API */
 #include "diskio.h"		/* Declarations of MAI */
 
